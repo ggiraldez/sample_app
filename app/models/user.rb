389ff613ff0c1a0e17cc7e2a6b612e5f2a774556
Gustavo Giráldez <gustavo.giraldez@gmail.com>
2011-04-18 23:50:41 -0300

    Created User model. Added some validation and unit tests

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+# Schema version: 20110419021709
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+
+  validates :name, :presence => true
+  validates :email, :presence => true
+end
