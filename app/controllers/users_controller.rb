0b6ba7a9eb2c266138c45b89856416e2435be618
Gustavo Giráldez <gustavo.giraldez@gmail.com>
2011-04-21 02:37:23 -0300

    Protected user editing actions using filters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -25,12 +27,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user fetched in correct_user filter
     @title = "Edit user"
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user fetched in correct_user filter
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated."
       redirect_to @user
@@ -39,5 +41,16 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 end
