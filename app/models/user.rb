ee368751dd47c5916914f4e5e5836e3b3a5157e2
Gustavo Giráldez <gustavo.giraldez@gmail.com>
2011-04-21 23:46:30 -0300

    Annotated User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110420031754
+# Schema version: 20110422010218
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 require 'digest'
