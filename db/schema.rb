05627413c61666b0f0d189946f0b768ad2628b2a
Gustavo Giráldez <gustavo.giraldez@gmail.com>
2011-04-24 23:27:24 -0300

    Added user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110422010218) do
+ActiveRecord::Schema.define(:version => 20110424225815) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id"], :name => "index_microposts_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "name"
