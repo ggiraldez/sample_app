22e885f12aba13593d79c66736b0018a4af9e355
Gustavo Giráldez <gustavo.giraldez@gmail.com>
2011-04-27 00:05:31 -0300

    Added javascript character counter for the new micropost textarea

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,35 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+(function() {
+
+  document.observe('dom:loaded', function() {
+    $$('textarea#micropost_content').each(function(field) {
+      var label;
+
+      function format(count) {
+        if (count == 1) {
+          return "1 character left";
+        } else {
+          return count + " characters left";
+        }
+      }
+
+      label = document.createElement('span');
+      label.addClassName('remaining');
+      label.innerHTML = format(140);
+      field.insert({ after: label });
+      
+      field.on('keyup', function(evt) {
+        var count = 140 - $F(field).length;
+        label.innerHTML = format(count);
+        if (count > 0) {
+          label.removeClassName('nomore');
+        } else {
+          label.addClassName('nomore');
+        }
+      });
+    });
+  });
+
+}());
+
